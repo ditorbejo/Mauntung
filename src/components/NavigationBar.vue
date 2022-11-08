4e91d1806f2ce1d7241d5ebc1f7674dbbc390551
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-11-09 00:44:40 +0700

    refactor navigation bar component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,27 +1,49 @@
 <script setup>
 import { RouterLink } from "vue-router";
 import NavigationItem from "./NavigationItem.vue";
+
+const navItems = [
+  {
+    to: "home",
+    isActive: true,
+    icon: "home",
+    label: "Beranda",
+  },
+  {
+    to: "history",
+    isActive: false,
+    icon: "history",
+    label: "Riwayat",
+  },
+  {
+    to: "report",
+    isActive: false,
+    icon: "assignment",
+    label: "Laporan",
+  },
+  {
+    to: "profile",
+    isActive: false,
+    icon: "person",
+    label: "Profil",
+  },
+];
 </script>
 
 <template>
   <nav
     class="flex justify-between px-6 py-2.5 fixed bottom-0 left-0 right-0 shadow-bottomBarShadow"
   >
-    <RouterLink :to="{ name: 'home' }">
-      <NavigationItem :isActive="true" :icon="'home'" :label="'Beranda'" />
-    </RouterLink>
-    <RouterLink :to="{ name: 'history' }">
-      <NavigationItem :isActive="false" :icon="'history'" :label="'Riwayat'" />
-    </RouterLink>
-    <RouterLink :to="{ name: 'report' }">
+    <RouterLink
+      v-for="(navItem, index) in navItems"
+      :to="{ name: navItem.to }"
+      :key="'link-' + index"
+    >
       <NavigationItem
-        :isActive="false"
-        :icon="'assignment'"
-        :label="'Laporan'"
+        :isActive="navItem.isActive"
+        :icon="navItem.icon"
+        :label="navItem.label"
       />
-    </RouterLink>
-    <RouterLink :to="{ name: 'profile' }">
-      <NavigationItem :isActive="false" :icon="'person'" :label="'Profil'" />
     </RouterLink>
   </nav>
 </template>
