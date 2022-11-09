acbcb94ae61a9582a12b3989c17dd07fae32aaf8
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-11-09 09:13:59 +0700

    adjust navigation item styles

--- src/components/NavigationItem.vue
+++ src/components/NavigationItem.vue
@@ -4,10 +4,19 @@ const props = defineProps(["isActive", "icon", "label"]);
 
 <template>
   <div
-    class="flex flex-col items-center"
+    class="flex flex-col items-center justify-between w-12 h-12"
     :class="[props.isActive ? 'text-biru2 font-semibold' : 'text-biru1']"
   >
-    <span class="material-symbols-rounded"> {{ props.icon }}</span>
-    {{ props.label }}
+    <div
+      class="py-1 w-full flex flex-col items-center rounded-full"
+      :class="[props.isActive ? 'bg-biru2' : '']"
+    >
+      <span
+        class="material-symbols-rounded text-center"
+        :class="[props.isActive ? 'text-white' : '']"
+        >{{ props.icon }}</span
+      >
+    </div>
+    <span class="text-xs font-medium">{{ props.label }}</span>
   </div>
 </template>
