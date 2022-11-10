4329af54ef36041569046251581b241bc919950c
ditorbejo <ditoooriq@gmail.com>
2022-11-10 23:02:43 +0700

    membuat komponen text field

---
+++ src/components/TextField.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(["label", "placeholder", "typeInput", "nameInput"]);
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <label class="font-medium" for="">{{ props.label }}</label>
+    <input
+      class="flex items-center px-3 py-3 border border-solid rounded-lg bg-biru6 border-biru1 text-biru1"
+      :type="props.typeInput"
+      :name="props.nameInput"
+      :placeholder="props.placeholder"
+    />
+  </div>
+</template>
+
+<style scoped>
+input::placeholder {
+  @apply text-gray;
+}
+</style>
