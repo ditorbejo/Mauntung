0b648ee625f3dd5ab7bcf0bfabf8c7505a3bb950
ditorbejo <ditoooriq@gmail.com>
2022-11-10 23:01:51 +0700

    membuat komponen button group

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+const props = defineProps(["active"]);
+</script>
+
+<template>
+<div class="flex w-full">
+    <button class="basis-1/2 rounded-l-full border border-solid border-biru2 border-2 text-center py-1 text-white font-medium" :class="[active == 'left' ? 'bg-biru2' : 'bg-biru4']" >Email</button>
+    <button class="basis-1/2 rounded-r-full border border-solid border-biru2 border-2 text-center py-1 text-white font-medium" :class="[active == 'right' ? 'bg-biru2' : 'bg-biru4']" >No. Hp</button>
+</div>
+</template>
