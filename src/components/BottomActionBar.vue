2e94b909b3fa3311ecaf3eb4c4e86150dff4ec8d
ditorbejo <ditoooriq@gmail.com>
2022-11-10 21:08:51 +0700

    membuat komponen bottom action

---
+++ src/components/BottomActionBar.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps(["actionName"]);
+</script>
+
+<template>
+  <div class="flex w-full gap-2.5 py-2.5 px-4 items-center justify-between fixed bottom-0 shadow-bottomBarShadow bg-white">
+    <slot></slot>
+    <button class="btn btn-primary btn-rounded btn-base">
+      {{ props.actionName }}
+    </button>
+  </div>
+</template>
