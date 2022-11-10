ed63ddfd4611aa9434d8a69ffb86dcb31ef0cb7b
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-11-10 21:16:28 +0700

    membuat komponen alert card

---
+++ src/components/AlertCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps(["title", "content", "icon"]);
+</script>
+
+<template>
+  <div class="border border-biru1 flex bg-biru6 rounded-lg p-3 text-biru1">
+    <span class="material-symbols-rounded mt-1"> {{ props.icon }} </span>
+    <div class="ml-2">
+      <div class="text-lg font-semibold">{{ props.title }}</div>
+      <p class="text-sm">{{ props.content }}</p>
+    </div>
+  </div>
+</template>
