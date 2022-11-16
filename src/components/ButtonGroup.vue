996599f631863fa046fa656df9f46a06a116ef09
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-11-16 12:13:34 +0700

    remove unused style

--- src/components/ButtonGroup.vue
+++ src/components/ButtonGroup.vue
@@ -7,14 +7,14 @@ const emits = defineEmits(["leftClick", "rightClick"]);
   <div class="flex w-full">
     <button
       @click="$emit('leftClick')"
-      class="basis-1/2 rounded-l-full border border-solid border-biru2 border-2 text-center py-1 text-white font-medium"
+      class="basis-1/2 rounded-l-full border-2 border-solid border-biru2 text-center py-1 text-white font-medium"
       :class="[props.active == 'left' ? 'bg-biru2' : 'bg-white text-biru2']"
     >
       {{ props.leftText }}
     </button>
     <button
       @click="$emit('rightClick')"
-      class="basis-1/2 rounded-r-full border border-solid border-biru2 border-2 text-center py-1 text-white font-medium"
+      class="basis-1/2 rounded-r-full border-2 border-solid border-biru2 text-center py-1 text-white font-medium"
       :class="[props.active == 'right' ? 'bg-biru2' : 'bg-white text-biru2']"
     >
       {{ props.rightText }}
