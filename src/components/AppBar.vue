8b1eff47ea0aa660ade5af2719fd7c87df616ab1
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-11-23 09:40:16 +0700

    implement go back in app bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,12 +1,22 @@
 <script setup>
+import { useRouter } from "vue-router";
+
 const props = defineProps(["icon", "title"]);
+const router = useRouter();
+
+const goBack = () => {
+  router.go(-1);
+};
 </script>
 
 <template>
   <header
     class="flex w-full py-2.5 px-4 gap-6 items-center sticky top-0 z-40 shadow-topBarShadow bg-white"
   >
-    <button class="flex items-center bg-biru2 rounded-full py-1 px-1">
+    <button
+      class="flex items-center bg-biru2 rounded-full py-1 px-1"
+      @click="goBack"
+    >
       <span class="material-symbols-rounded text-white">
         {{ props.icon }}
       </span>
