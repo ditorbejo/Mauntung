a97c3ccd2c8aa703884e00ed6a8f8d2e49fa0cf1
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-11-24 14:21:09 +0700

    adjust appbar button styling

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -8,10 +8,10 @@ const emits = defineEmits(["buttonClick"]);
     class="flex w-full py-2.5 px-4 gap-6 items-center sticky top-0 z-40 shadow-topBarShadow bg-white"
   >
     <button
-      class="flex items-center bg-biru2 rounded-full py-1 px-1"
+      class="flex items-center bg-biru2 rounded-full py-2.5 px-2.5"
       @click="$emit('buttonClick')"
     >
-      <span class="material-symbols-rounded text-white">
+      <span class="material-symbols-rounded text-white text-base leading-none">
         {{ props.icon }}
       </span>
     </button>
