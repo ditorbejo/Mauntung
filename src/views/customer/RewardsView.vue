9c21f1a63fe17f4b5ef65fbe4f4c9d44cdcada2e
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-11-24 09:57:27 +0700

    implements customer reward page

--- src/views/customer/RewardsView.vue
+++ src/views/customer/RewardsView.vue
@@ -1,5 +1,74 @@
-<script setup></script>
+<script setup>
+import AppBar from "../../components/AppBar.vue";
+import TextField from "../../components/TextField.vue";
+import RedeemCard from "../../components/RedeemCard.vue";
+
+const redeemCards = [
+  {
+    rewardName: "1 Kali Cuci Gratis",
+    isUsed: false,
+    date: new Date(2022, 11, 1),
+    programImg: "https://via.placeholder.com/36",
+    programName: "Laundree",
+  },
+  {
+    rewardName: "1 Kali Cuci Gratis",
+    isUsed: true,
+    date: new Date(2022, 11, 1),
+    programImg: "https://via.placeholder.com/36",
+    programName: "Laundree",
+  },
+  {
+    rewardName: "1 Kali Cuci Gratis",
+    isUsed: false,
+    date: new Date(2022, 10, 1),
+    programImg: "https://via.placeholder.com/36",
+    programName: "Laundree",
+  },
+  {
+    rewardName: "1 Kali Cuci Gratis",
+    isUsed: false,
+    date: new Date(2022, 10, 1),
+    programImg: "https://via.placeholder.com/36",
+    programName: "Laundree",
+  },
+  {
+    rewardName: "1 Kali Cuci Gratis",
+    isUsed: false,
+    date: new Date(2022, 10, 1),
+    programImg: "https://via.placeholder.com/36",
+    programName: "Laundree",
+  },
+  {
+    rewardName: "1 Kali Cuci Gratis",
+    isUsed: false,
+    date: new Date(2022, 10, 1),
+    programImg: "https://via.placeholder.com/36",
+    programName: "Laundree",
+  },
+  {
+    rewardName: "1 Kali Cuci Gratis",
+    isUsed: false,
+    date: new Date(2022, 10, 1),
+    programImg: "https://via.placeholder.com/36",
+    programName: "Laundree",
+  },
+];
+</script>
 
 <template>
-  <main>Rewards</main>
+  <main>
+    <AppBar icon="arrow_back" title="Daftar Reward" />
+    <div class="px-7.5 pt-5 pb-20">
+      <TextField placeholder="Cari merchant/brand..." type-input="text" />
+
+      <div class="flex flex-col gap-3 mt-8 items-center">
+        <RedeemCard
+          v-for="(card, index) in redeemCards"
+          :key="index"
+          v-bind="card"
+        />
+      </div>
+    </div>
+  </main>
 </template>
